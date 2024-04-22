07d017b09c7d3996e470cccfca2b77742c3582c2
ougege <1258947325@qq.com>
2024-04-22 13:21:17 +0800

    feat: 增加文档地址

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,10 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+// https://antdv.com/components/steps-cn
 import Antd from 'ant-design-vue'
-import 'animate.css'
 import 'ant-design-vue/dist/reset.css'
+// https://animate.style/
+import 'animate.css'
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
