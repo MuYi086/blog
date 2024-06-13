fce3d3e25c87f3610059b05b6c053747c2354435
muyi086 <1258947325@qq.com>
2024-06-14 00:34:42 +0800

    chore: 先注释html压缩,仅压缩js

--- compress_gz.sh
+++ compress_gz.sh
@@ -20,60 +20,60 @@ gzip -9 ./*.js
 # 退出回到dist
 cd ../
 
-# 进入dist/Snippets/zsh目录
-cd Snippets/zsh
-gzip -9 ./*.html
-# 退出回到dist
-cd ../../
+# # 进入dist/Snippets/zsh目录
+# cd Snippets/zsh
+# gzip -9 ./*.html
+# # 退出回到dist
+# cd ../../
 
-# 进入dist/Docs
-cd Docs
-# 处理AI目录
-cd AI
-gzip -9 ./*.html
-cd ../
-# 处理Windows目录
-cd Windows
-gzip -9 ./*.html
-cd ../
-# 处理Git目录
-cd Git
-gzip -9 ./*.html
-cd ../
-# 处理Essay目录
-cd Essay
-gzip -9 ./*.html
-cd ../
-# 处理Back-End/Node目录
-cd Back-End/Node
-gzip -9 ./*.html
-# 处理Back-End/Python目录
-cd ../Python
-gzip -9 ./*.html
-cd ../../
-# 处理Front-End/CSS目录
-cd Front-End/CSS
-gzip -9 ./*.html
-# 处理Front-End/JS目录
-cd ../JS
-gzip -9 ./*.html
-# 处理Front-End/Standards目录
-cd ../Standards
-gzip -9 ./*.html
-cd ../../
-# 处理Linux/Deepin目录
-cd Linux/Deepin
-gzip -9 ./*.html
-# 处理Linux/Docker目录
-cd ../Docker
-gzip -9 ./*.html
-# 处理Linux/Shell目录
-cd ../Shell
-gzip -9 ./*.html
-# 处理Linux/Wall目录
-cd ../Wall
-gzip -9 ./*.html
-cd ../../
+# # 进入dist/Docs
+# cd Docs
+# # 处理AI目录
+# cd AI
+# gzip -9 ./*.html
+# cd ../
+# # 处理Windows目录
+# cd Windows
+# gzip -9 ./*.html
+# cd ../
+# # 处理Git目录
+# cd Git
+# gzip -9 ./*.html
+# cd ../
+# # 处理Essay目录
+# cd Essay
+# gzip -9 ./*.html
+# cd ../
+# # 处理Back-End/Node目录
+# cd Back-End/Node
+# gzip -9 ./*.html
+# # 处理Back-End/Python目录
+# cd ../Python
+# gzip -9 ./*.html
+# cd ../../
+# # 处理Front-End/CSS目录
+# cd Front-End/CSS
+# gzip -9 ./*.html
+# # 处理Front-End/JS目录
+# cd ../JS
+# gzip -9 ./*.html
+# # 处理Front-End/Standards目录
+# cd ../Standards
+# gzip -9 ./*.html
+# cd ../../
+# # 处理Linux/Deepin目录
+# cd Linux/Deepin
+# gzip -9 ./*.html
+# # 处理Linux/Docker目录
+# cd ../Docker
+# gzip -9 ./*.html
+# # 处理Linux/Shell目录
+# cd ../Shell
+# gzip -9 ./*.html
+# # 处理Linux/Wall目录
+# cd ../Wall
+# gzip -9 ./*.html
+# cd ../../
 
 
 
