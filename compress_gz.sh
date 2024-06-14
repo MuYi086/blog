53a53fe7c6a6fb5d3877e7bbf2fbdddf327ad935
ougege <1258947325@qq.com>
2024-06-14 17:56:05 +0800

    feat: gzip命令改造保留原文件

--- compress_gz.sh
+++ compress_gz.sh
@@ -8,72 +8,72 @@ cd ./.vitepress/dist
 # do
 #   gzip -k -f -9 $file
 # done
-gzip -9 ./*.html
+gzip -k -f -9 ./*.html
 
 # 进入dist/assets/chunks
 cd assets/chunks
-gzip -9 ./*.js
+gzip -k -f -9 ./*.js
 # 退出回到assets
 cd ../
 # 处理assets目录下所有js文件
-gzip -9 ./*.js
+gzip -k -f -9 ./*.js
 # 退出回到dist
 cd ../
 
-# # 进入dist/Snippets/zsh目录
-# cd Snippets/zsh
-# gzip -9 ./*.html
-# # 退出回到dist
-# cd ../../
+# 进入dist/Snippets/zsh目录
+cd Snippets/zsh
+gzip -k -f -9 ./*.html
+# 退出回到dist
+cd ../../
 
-# # 进入dist/Docs
-# cd Docs
-# # 处理AI目录
-# cd AI
-# gzip -9 ./*.html
-# cd ../
-# # 处理Windows目录
-# cd Windows
-# gzip -9 ./*.html
-# cd ../
-# # 处理Git目录
-# cd Git
-# gzip -9 ./*.html
-# cd ../
-# # 处理Essay目录
-# cd Essay
-# gzip -9 ./*.html
-# cd ../
-# # 处理Back-End/Node目录
-# cd Back-End/Node
-# gzip -9 ./*.html
-# # 处理Back-End/Python目录
-# cd ../Python
-# gzip -9 ./*.html
-# cd ../../
-# # 处理Front-End/CSS目录
-# cd Front-End/CSS
-# gzip -9 ./*.html
-# # 处理Front-End/JS目录
-# cd ../JS
-# gzip -9 ./*.html
-# # 处理Front-End/Standards目录
-# cd ../Standards
-# gzip -9 ./*.html
-# cd ../../
-# # 处理Linux/Deepin目录
-# cd Linux/Deepin
-# gzip -9 ./*.html
-# # 处理Linux/Docker目录
-# cd ../Docker
-# gzip -9 ./*.html
-# # 处理Linux/Shell目录
-# cd ../Shell
-# gzip -9 ./*.html
-# # 处理Linux/Wall目录
-# cd ../Wall
-# gzip -9 ./*.html
-# cd ../../
+# 进入dist/Docs
+cd Docs
+# 处理AI目录
+cd AI
+gzip -k -f -9 ./*.html
+cd ../
+# 处理Windows目录
+cd Windows
+gzip -k -f -9 ./*.html
+cd ../
+# 处理Git目录
+cd Git
+gzip -k -f -9 ./*.html
+cd ../
+# 处理Essay目录
+cd Essay
+gzip -k -f -9 ./*.html
+cd ../
+# 处理Back-End/Node目录
+cd Back-End/Node
+gzip -k -f -9 ./*.html
+# 处理Back-End/Python目录
+cd ../Python
+gzip -k -f -9 ./*.html
+cd ../../
+# 处理Front-End/CSS目录
+cd Front-End/CSS
+gzip -k -f -9 ./*.html
+# 处理Front-End/JS目录
+cd ../JS
+gzip -k -f -9 ./*.html
+# 处理Front-End/Standards目录
+cd ../Standards
+gzip -k -f -9 ./*.html
+cd ../../
+# 处理Linux/Deepin目录
+cd Linux/Deepin
+gzip -k -f -9 ./*.html
+# 处理Linux/Docker目录
+cd ../Docker
+gzip -k -f -9 ./*.html
+# 处理Linux/Shell目录
+cd ../Shell
+gzip -k -f -9 ./*.html
+# 处理Linux/Wall目录
+cd ../Wall
+gzip -k -f -9 ./*.html
+cd ../../
 
 
 
